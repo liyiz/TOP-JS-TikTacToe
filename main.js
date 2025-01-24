b83c01c961ce4e618fd1a699db4e2c97a90ccefd
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2025-01-24 16:07:10 -0500

    Add multiple functions check description
    Add Cell factory function
    Add initGrid logic
    Add player1 and player 2 definitions
    Define gridsize

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Module/IIFE pattern for scoping
-    const App = (() => {
-      // Private variables/functions
-      const init = () => { console.log("Hello, World!") }
-      return {
-        // Public methods (e.g., init)
-        init
-      };
+    const Game = (() => {
+        // Private variables/functions
+        const init = () => { console.log("Hello, World!") }
+
+        const gridsize = 3;
+        const board = [];
+
+        const initGrid = (gridsize) => {
+            // Reset the grid
+            for (let i = 0; i < gridsize; i++) {
+                board[i] = [];
+                for (let j = 0; j < gridsize; j++) {
+                    board[i].push(Cell());
+                }
+            }
+        }
+
+        function Cell() {
+            
+            let value = 0;
+          
+            // Accept a player's token to change the value of the cell
+            const addToken = (player) => {
+                value = player;
+            };
+          
+            // How we will retrieve the current value of this cell through closure
+            const getValue = () => value;
+          
+            return {
+                addToken,
+                getValue
+            };
+        }
+
+        const checkWinner = () => {
+            // Scan the grid for a winning line
+        }
+
+        const playPiece = (gridcell, pieceType) => {
+            // Place either a 'o' or 'x' in the selected grid cell
+        }
+
+
+
+        return {
+            // Public methods (e.g., init)
+            init,
+            initGrid,
+            checkWinner,
+            playPiece
+        };
     })();
   
-    App.init(); // Start the app
+    Game.init(); // Start the app
   });
