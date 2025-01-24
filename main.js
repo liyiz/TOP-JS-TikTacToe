da760ccd01c35c4cf2167f42f398b17890259459
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2025-01-24 15:17:57 -0500

    Add initial module and script startup

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+document.addEventListener('DOMContentLoaded', () => {
+    // Module/IIFE pattern for scoping
+    const App = (() => {
+      // Private variables/functions
+      const init = () => { console.log("Hello, World!") }
+      return {
+        // Public methods (e.g., init)
+        init
+      };
+    })();
+  
+    App.init(); // Start the app
+  });
